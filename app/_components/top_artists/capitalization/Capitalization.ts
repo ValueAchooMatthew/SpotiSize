1ab68f094bf729717e32b2014c34c2b7a0c2a673
ValueAchooMatthew <78985742+ValueAchooMatthew@users.noreply.github.com>
2023-12-12 23:48:37 -0500

    Updated capitalization function in case of no listed genre

--- app/_components/top_artists/capitalization/Capitalization.ts
+++ app/_components/top_artists/capitalization/Capitalization.ts
@@ -1,19 +1,25 @@
-export default function capitalizeFirstLetter(phrase: string){
-    phrase = phrase.trim();
-    const word = phrase.split("");
-    const len = word.length;
-    if(len >= 1){
-        word[0] = word[0].toUpperCase();
-        for(let i = 1; i < len; i++ ){
-            if(word[i] != " " && word[i-1] == " "){
-                word[i] = word[i].toUpperCase();
+export default function capitalizeFirstLetter(phrase: string | undefined): string{
+    if(phrase){
+        phrase = phrase.trim();
+        const word = phrase.split("");
+        const len = word.length;
+        if(len >= 1){
+            word[0] = word[0].toUpperCase();
+            for(let i = 1; i < len; i++ ){
+                if(word[i] != " " && word[i-1] == " "){
+                    word[i] = word[i].toUpperCase();
+                }
             }
+            return word.join("");
+    
         }
-        return word.join("");
+    
+        else{
+            return ("No Listed Genre")
+        }
 
+    }else{
+        return ("No Listed Genre");
     }
 
-    else{
-        return ("No Listed Genre")
-    }
 }
