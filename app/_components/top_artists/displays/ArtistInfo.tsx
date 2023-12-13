b5729411b358cad6d740c414bfc4080b436d21e2
Max T <maxtyuterev@gmail.com>
2023-12-13 00:18:22 -0500

    make artist images load eagerly

--- app/_components/top_artists/displays/ArtistInfo.tsx
+++ app/_components/top_artists/displays/ArtistInfo.tsx
@@ -2,12 +2,12 @@ import Link from "next/link"
 import Image from "next/image"
 import { LocalTrack, RegularTrack } from "@/app/_types/data"
 
-export default function ArtistInfo({view, information, limit}: {view: "artist_view" | "track_view", information: RegularTrack | LocalTrack | undefined, limit: number} ){
+export default function ArtistInfo({ view, information, limit }: { view: "artist_view" | "track_view", information: RegularTrack | LocalTrack | undefined, limit: number }) {
 
-    return(
+    return (
         <>
 
-            {view == "artist_view"? 
+            {view == "artist_view" ?
                 <span className={"md:text-4xl text-3xl text-fontBlue text-center font-semibold"}>
                     Artist Info
                 </span>
@@ -16,13 +16,13 @@ export default function ArtistInfo({view, information, limit}: {view: "artist_vi
                     Track Info
                 </span>
             }
-            {information? 
+            {information ?
                 <div className="flex justify-between align-middle md:mt-8 mt-4 ">
                     <div className="self-center flex justify-center overflow-hidden md:w-40 md:h-40 w-24 h-24 rounded-2xl mx-6 flex-shrink-0">
-                    {/* Displays spotify icon and reroutes to page if track is local*/}
-                    <Link href={{pathname: information.is_local? "":information.page}}>
-                        <Image className = "md:w-40 md:h-40 w-24 h-24 object-cover overflow-hidden transition-all duration-300 " width={1000} height={1000} src={information.is_local? "/img/Spotify_icon.svg":information.img} alt={"spotify page photo"}></Image>
-                    </Link>
+                        {/* Displays spotify icon and reroutes to page if track is local*/}
+                        <Link href={{ pathname: information.is_local ? "" : information.page }}>
+                            <Image priority={true} className="md:w-40 md:h-40 w-24 h-24 object-cover overflow-hidden transition-all duration-300 " width={1000} height={1000} src={information.is_local ? "/img/Spotify_icon.svg" : information.img} alt={"spotify page photo"}></Image>
+                        </Link>
                     </div>
                     <div className="flex flex-col self-center justify-center w-full">
                         <span className="md:text-3xl text-xl md:mb-4">
@@ -30,9 +30,9 @@ export default function ArtistInfo({view, information, limit}: {view: "artist_vi
                         </span>
                         {/* Checking if song/artist is local, if so displays information based on whether it is in track or artist view, 
                         otherwise, display nothing */}
-                        {!information.is_local?
+                        {!information.is_local ?
 
-                            (view == "artist_view"?
+                            (view == "artist_view" ?
                                 (<span className=" md:text-base text-xs">
                                     Genre: {information.group}
                                 </span>)
@@ -52,7 +52,7 @@ export default function ArtistInfo({view, information, limit}: {view: "artist_vi
                 </div>
                 :
                 <span className="text-black md:text-3xl text-lg md:mt-12 mt-6">
-                    Click on an artist to see more information    
+                    Click on an artist to see more information
                 </span>
             }
         </>
