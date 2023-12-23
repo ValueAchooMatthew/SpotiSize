f5595f55c3ebea1894f3ddb0185ab36f27d7f096
Max T <maxtyuterev@gmail.com>
2023-12-22 23:13:10 -0500

    get rid of react error about data attrib

--- app/_components/top_artists/displays/TimeFramePicker.tsx
+++ app/_components/top_artists/displays/TimeFramePicker.tsx
@@ -2,8 +2,7 @@
 import { Dispatch, SetStateAction } from "react";
 import { LocalTrack, RegularTrack, TimeFrame } from "@/app/_types/data";
 
-export default function TimeFramePicker({ timeFrame, setTimeFrame, setInformation }:
-{
+export default function TimeFramePicker({ timeFrame, setTimeFrame, setInformation }: {
   timeFrame: TimeFrame,
   setTimeFrame: Dispatch<SetStateAction<TimeFrame>>,
   setInformation: Dispatch<SetStateAction<LocalTrack | RegularTrack | undefined>>;
@@ -12,23 +11,23 @@ export default function TimeFramePicker({ timeFrame, setTimeFrame, setInformatio
   return (
     <div className="text-center h-fit ">
       <span className="md:text-3xl text-2xl text-fontBlue font-semibold">
-                Adjust Timeframe
+        Adjust Timeframe
       </span>
       <div className="flex flex-col 2xl:flex-row md:mt-4 mt-2 w-full justify-around md:text-2xl text-base">
         {/* Setting information only to undefined if user is actually changing into a different time range, otherwise 
                 results in unnecessary updates in the DOM */}
         {/* Setting id to selected range for better tailwind selector integration */}
-        <button data-timeFrame={timeFrame} className="transition-all duration-300 md:py-0.5 hover:scale-110 data-[timeFrame='short\_term']:text-purple"
+        <button data-timeframe={timeFrame} className="transition-all duration-300 md:py-0.5 hover:scale-110 data-[timeframe='short\_term']:text-purple"
           onClick={() => { if (timeFrame != "short_term") { setInformation(undefined); } setTimeFrame("short_term"); }}>
-                    Short Term
+          Short Term
         </button>
-        <button data-timeFrame={timeFrame} className="transition-all duration-300 md:py-0.5 hover:scale-110 data-[timeFrame='medium\_term']:text-purple"
+        <button data-timeframe={timeFrame} className="transition-all duration-300 md:py-0.5 hover:scale-110 data-[timeframe='medium\_term']:text-purple"
           onClick={() => { if (timeFrame != "medium_term") { setInformation(undefined); } setTimeFrame("medium_term"); }}>
-                    Medium Term
+          Medium Term
         </button>
-        <button data-timeFrame={timeFrame} className="transition-all duration-300 md:py-0.5 hover:scale-110 data-[timeFrame='long\_term']:text-purple"
+        <button data-timeframe={timeFrame} className="transition-all duration-300 md:py-0.5 hover:scale-110 data-[timeframe='long\_term']:text-purple"
           onClick={() => { if (timeFrame != "long_term") { setInformation(undefined); } setTimeFrame("long_term"); }}>
-                    Long Term
+          Long Term
         </button>
       </div>
     </div>
--- app/_components/top_artists/displays/TopItemsVariantPicker.tsx
+++ app/_components/top_artists/displays/TopItemsVariantPicker.tsx
@@ -3,8 +3,7 @@ import { LocalTrack, RegularTrack, TopItemsVariant } from "@/app/_types/data";
 import { Dispatch, SetStateAction } from "react";
 
 
-export default function TopItemsVariantPicker({ topItemsVariant, setTopItemsVariant, setInformation }:
-{
+export default function TopItemsVariantPicker({ topItemsVariant, setTopItemsVariant, setInformation }: {
   topItemsVariant: TopItemsVariant,
   setTopItemsVariant: Dispatch<SetStateAction<TopItemsVariant>>
   setInformation: Dispatch<SetStateAction<LocalTrack | RegularTrack | undefined>>
@@ -13,25 +12,25 @@ export default function TopItemsVariantPicker({ topItemsVariant, setTopItemsVari
   return (
     <div className="text-center h-fit">
       <span className="md:text-3xl text-2xl text-fontBlue font-semibold">
-                View
+        View
       </span>
       <div className="flex flex-col 2xl:flex-row md:mt-4 mt-2 w-full justify-around md:text-2xl text-base">
         {/* Setting information only to undefined if user is actually changing into a different view, otherwise 
                 results in unnecessary updates in the DOM */}
-        <button data-topItemsVariant={topItemsVariant}
-          className="transition-all duration-300 py-0.5 hover:scale-110 data-[topItemsVariant='artist\_view']:text-purple"
+        <button data-topitemsvariant={topItemsVariant}
+          className="transition-all duration-300 py-0.5 hover:scale-110 data-[topitemsvariant='artist\_view']:text-purple"
           onClick={() => {
             if (topItemsVariant != "artist_view") { setInformation(undefined); }
             setTopItemsVariant("artist_view");
           }}>
-                    Artist View
+          Artist View
         </button>
-        <button data-topItemsVariant={topItemsVariant}
-          className="transition-all duration-300 py-0.5 hover:scale-110 data-[topItemsVariant='track\_view']:text-purple"
+        <button data-topitemsvariant={topItemsVariant}
+          className="transition-all duration-300 py-0.5 hover:scale-110 data-[topitemsvariant='track\_view']:text-purple"
           onClick={() => {
             if (topItemsVariant != "track_view") { setInformation(undefined); } setTopItemsVariant("track_view");
           }}>
-                    Track View
+          Track View
         </button>
       </div>
     </div>
