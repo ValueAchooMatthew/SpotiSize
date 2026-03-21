2d4944aebbf98ba18d4fe3229d1e1b36e2fb9faa
Maxim Tyuterev <maxtyuterev@gmail.com>
2026-03-21 11:40:14 -0400

    small linter errors and nots to self

--- app/_components/login_button/LoginButton.tsx
+++ app/_components/login_button/LoginButton.tsx
@@ -5,7 +5,7 @@ import { signIn } from "@/auth-client";
 
 export default function LoginButton() {
 
-  return (
+  return (                                 // TODO(MT): add these to the global pallet
         <button onClick={signIn} className="bg-[#9e8afe] md:px-12 md:py-8 px-6 py-4 text-center flex justify-center self-center 
           rounded-full shadow-2xl mx-auto mt-8 z-10" >
             <span className="font-jost md:text-4xl text-2xl text-white font-bold" >
--- app/_components/logout_button/LogoutButton.tsx
+++ app/_components/logout_button/LogoutButton.tsx
@@ -3,7 +3,7 @@ import { signOut } from "@/auth-client";
 import Link from "next/link";
 
 export default function LogoutButton() {
-  return (
+  return (                                                                                                                                                          // TODO(MT): add these to the global pallet
         <Link href="/" onNavigate={signOut} className="bg-lightPurple hover:bg-yellow text-white xl:text-2xl text-lg font-bold font-jost py-2 px-4 rounded-full bg-[#6545FF] w-full h-10 rounded-full mx-auto">
             Log Out
         </Link>)
