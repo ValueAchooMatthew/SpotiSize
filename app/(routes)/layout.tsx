55e045b70d7c9be50a73d24fd04d1e2ee2cd5658
Maxim Tyuterev <maxtyuterev@gmail.com>
2026-03-21 11:40:14 -0400

    small linter errors and nots to self

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -1,8 +1,8 @@
-import Navbar from "../_components/Navbar";
+import Navbar from "../_components/navbar/Navbar";
 import "../globals.css";
 import { Inter } from "next/font/google";
 import React from "react";
-
+import Image from "next/image";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -17,9 +17,14 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + " bg-gradient-to-b from-[#11005e] to-slate-950 min-h-screen h-fit min-w-fit w-full"} >
-        <Navbar></Navbar>
-        {children}
+      <body className={inter.className + " bg-linear-to-b from-background-primary to-background-secondary min-h-screen h-fit min-w-fit w-full"} >
+        <div className="flex flex-col grow">
+          <Navbar></Navbar>
+          <Image className="overflow-hidden absolute -left-64 -z-10" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"></Image>
+          <Image className="overflow-hidden absolute -top-64 -right-80 -z-10" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"></Image>
+          {children}
+
+        </div>
       </body>
     </html>
   );
--- app/(routes)/page.tsx
+++ app/(routes)/page.tsx
@@ -6,8 +6,7 @@ export default function Home() {
 
   return (
     <main className="relative min-h-screen h-full pb-40 overflow-hidden">
-      <Image className="absolute -left-64 -z-10" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"></Image>
-      <Image className="absolute -top-64 -right-80 -z-10" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"></Image>
+      
       {/* <Image className="absolute -top-56 right-0 w-[40rem] h-[40rem]" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"></Image> */}
       <Image className="absolute hidden md:inline-block top-20 2xl:right-64 lg:right-32 right-12 lg:w-72 lg:h-72 w-56 h-56 -z-10" width={1000} height={1000} src={"/img/saturn.svg"} alt="saturn image" ></Image>
       <Image className="absolute hidden 2xl:inline-block bottom-1/2 right-32 w-48 h-48 -z-10" width={1000} height={1000} src={"/img/Long cloud.svg"} alt="cloud"></Image>
@@ -18,15 +17,15 @@ export default function Home() {
 
 
       <div className="mt-32 relative w-fit h-fit mx-auto font-jost">
-        <h3 className="absolute text-fontBlue md:text-6xl medsmal:text-4xl sm:text-2xl text-xl text-center medsmal:top-10 md:top-14 top-16 md:-left-60 medsmal:-left-28 xs:-left-20 -left-16">
+        <h3 className="absolute text-secondary md:text-6xl medsmal:text-4xl sm:text-2xl text-xl text-center medsmal:top-10 md:top-14 top-16 md:-left-60 medsmal:-left-28 xs:-left-20 -left-16">
           Welcome to
         </h3>
         <div className="w-fit h-fit relative">
-          <h1 className="font-bold md:text-[7.5rem] medsmal:text-[5.5rem] text-[3.5rem] text-fontBlue text-center absolute md:top-[7.1rem] top-[5.0rem] md:-left-14 medsmal:-left-[3.2rem] xs:-left-[1.8rem] -left-[1.2rem] z-10">
+          <h1 className="font-bold md:text-[7.5rem] medsmal:text-[5.5rem] text-[3.5rem] text-secondary text-center absolute md:top-[7.1rem] top-[5.0rem] md:-left-14 medsmal:-left-[3.2rem] xs:-left-[1.8rem] -left-[1.2rem] z-10">
             Sp
           </h1>
           <Image width={1000} height={1000} className="md:w-96 md:h-80 medsmal:w-64 medsmal:h-56 w-56 h-48" src={"/img/spaceman.png"} alt="spaceman"></Image>
-          <h1 className="font-bold md:text-[7.5rem] medsmal:text-[5.5rem] text-[3.5rem] text-fontBlue text-center absolute md:top-[7.1rem] top-[5.0rem] md:-right-40 medsmal:-right-32 xs:-right-20 -right-16 z-10">
+          <h1 className="font-bold md:text-[7.5rem] medsmal:text-[5.5rem] text-[3.5rem] text-secondary text-center absolute md:top-[7.1rem] top-[5.0rem] md:-right-40 medsmal:-right-32 xs:-right-20 -right-16 z-10">
             tisize
           </h1>
         </div>
@@ -39,7 +38,8 @@ export default function Home() {
         </h4>
 
         <div className="relative w-fit mx-auto hover:-translate-y-4 transition-all duration-300 will-change-transform">
-          <LoginButton/>
+          <LoginButton/>    
+                             {/*  TODO(MT): add these to the global pallet*/}
           <div className="bg-[#6445ff] w-full md:h-24 h-16 rounded-full mx-auto absolute md:-bottom-3 -bottom-2 left-1 -z-10">
           </div>
         </div>
