5082bbfa0a287917e98d0122cc95e211b2d91056
Maxim Tyuterev <maxtyuterev@gmail.com>
2026-03-21 11:37:22 -0400

    Use shared layout to simplify the page code

---
+++ app/(routes)/(info_routes)/about/page.tsx
@@ -0,0 +1,13 @@
+
+export default function About() {
+  return (
+    <div>
+      <h1 className="text-center sm:text-7xl text-6xl font-bold text-primary mt-20 mb-12 font-jost">
+        About
+      </h1>
+      <p className="text-center font-bold md:text-4xl xs:text-3xl text-2xl text-secondary md:w-180 medsmal:w-[30rem] w-[20rem] mx-auto">
+        Welcome to Spotisize, an interactive website for visualizing your top artists and songs on spotify! If you enjoy this website, be sure to share it with your friends.
+      </p>
+    </div>
+  );
+}
