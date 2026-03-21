ae896475dc9eac29fab24a02f00a4c486daea6e4
Maxim Tyuterev <maxtyuterev@gmail.com>
2026-03-21 16:01:37 -0400

    okay log in and out is working as expected

--- app/_components/login_button/LoginButton.tsx
+++ app/_components/login_button/LoginButton.tsx
@@ -1,12 +1,17 @@
 "use client"
 
 import { signIn } from "@/auth-client";
+import { useRouter } from "next/navigation";
 // import { authClient } from "@/auth-client";
 
 export default function LoginButton() {
 
+  const router = useRouter()
+  
+
+
   return (                               
-        <button onClick={signIn} className="bg-button-primary md:px-12 md:py-8 px-6 py-4 text-center flex justify-center self-center 
+        <button onClick={() => { signIn().then(() => router.push('/')) }}    className="bg-button-primary md:px-12 md:py-8 px-6 py-4 text-center flex justify-center self-center 
           rounded-full shadow-2xl mx-auto mt-8 z-10" >
             <span className="font-jost md:text-4xl text-2xl text-white font-bold" >
               Log in With Spotify
