9ecea5de42e223d2a51f4815ee6395f4049c92d5
Maxim Tyuterev <maxtyuterev@gmail.com>
2026-03-21 15:46:33 -0400

    Okay almost clean but login and logout are strange

--- app/_components/logout_button/LogoutButton.tsx
+++ app/_components/logout_button/LogoutButton.tsx
@@ -1,10 +1,16 @@
 "use client"
-import { signOut } from "@/auth-client";
-import Link from "next/link";
+
+import { signOut } from "@/auth-client"
 
 export default function LogoutButton() {
   return (                                                                                                                                                          
-        <Link href="/" onNavigate={signOut} className="bg-lightPurple hover:bg-yellow text-white xl:text-2xl text-lg font-bold font-jost py-2 px-4 rounded-full bg-button-secondary w-full h-10 rounded-full mx-auto">
-            Log Out
-        </Link>)
+    <div className="relative w-fit mx-auto hover:-translate-y-4 transition-all duration-300 will-change-transform">
+        <button onClick={signOut}  className="bg-bad-secondary md:px-12 md:py-8 px-6 py-4 text-center flex justify-center self-center rounded-full shadow-2xl mx-auto mt-8 z-10" >
+            <span className="font-jost md:text-4xl text-2xl text-white font-bold" >
+                Log Out
+            </span>
+        </button>
+        <div className="bg-button-bad-primary w-full md:h-24 h-16 rounded-full mx-auto absolute md:-bottom-3 -bottom-2 left-1 -z-10">
+    </div>
+</div>)
 }
