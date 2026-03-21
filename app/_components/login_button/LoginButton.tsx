bc3d74a8817cec4cf825c93e0019a6e173c04b26
Maxim Tyuterev <maxtyuterev@gmail.com>
2026-03-21 12:08:01 -0400

    might aswell add all the colors

--- app/_components/login_button/LoginButton.tsx
+++ app/_components/login_button/LoginButton.tsx
@@ -5,8 +5,8 @@ import { signIn } from "@/auth-client";
 
 export default function LoginButton() {
 
-  return (                                 // TODO(MT): add these to the global pallet
-        <button onClick={signIn} className="bg-[#9e8afe] md:px-12 md:py-8 px-6 py-4 text-center flex justify-center self-center 
+  return (                               
+        <button onClick={signIn} className="bg-button-primary md:px-12 md:py-8 px-6 py-4 text-center flex justify-center self-center 
           rounded-full shadow-2xl mx-auto mt-8 z-10" >
             <span className="font-jost md:text-4xl text-2xl text-white font-bold" >
               Log in With Spotify
