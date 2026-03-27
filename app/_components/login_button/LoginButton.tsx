aa79c3f397e1bc8c6748d37a1dc34c8f8f88adc2
Maxim Tyuterev <maxtyuterev@gmail.com>
2026-03-27 12:12:07 -0400

    moved the button accent to the button component

--- app/_components/login_button/LoginButton.tsx
+++ app/_components/login_button/LoginButton.tsx
@@ -1,20 +1,25 @@
-"use client"
+"use client";
 
 import { signIn } from "@/auth-client";
 import { useRouter } from "next/navigation";
 // import { authClient } from "@/auth-client";
 
 export default function LoginButton() {
+  const router = useRouter();
 
-  const router = useRouter()
-  
-
-
-  return (                               
-        <button onClick={() => { signIn().then(() => router.push('/')) }}    className="bg-button-primary md:px-12 md:py-8 px-6 py-4 text-center flex justify-center self-center 
-          rounded-full shadow-2xl mx-auto mt-8 z-10" >
-            <span className="font-jost md:text-4xl text-2xl text-white font-bold" >
-              Log in With Spotify
-            </span>
-        </button>)
+  return (
+    <>
+      <button
+        onClick={() => {
+          signIn().then(() => router.push("/"));
+        }}
+        className="bg-button-primary md:px-12 md:py-8 px-6 py-4 text-center flex justify-center self-center rounded-full shadow-2xl mx-auto mt-8 z-10"
+      >
+        <span className="font-jost md:text-4xl text-2xl text-white font-bold">
+          Log in With Spotify
+        </span>
+      </button>
+      <div className="bg-button-secondary w-full md:h-24 h-16 rounded-full mx-auto absolute md:-bottom-3 -bottom-2 left-1 -z-10"/>
+    </>
+  );
 }
