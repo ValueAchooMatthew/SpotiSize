bfdc1af1ff08ad61665c0f0c59ac12e5904a5536
Maxim Tyuterev <maxtyuterev@gmail.com>
2026-03-27 11:44:30 -0400

    made navbar use flex

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -13,14 +13,13 @@ export const metadata = {
 export default function RootLayout(props: LayoutProps<"/">) {
   return (
     <html lang="en">
-      <body className={inter.className + " bg-linear-to-b from-background-primary to-background-secondary min-h-screen h-fit min-w-fit w-full"} >
+      <body className={inter.className + " bg-linear-to-b from-background-primary to-background-secondary"} >
         <div className="flex flex-col grow">
-          <Navbar></Navbar>
-          <Image className="overflow-hidden absolute -left-64 -z-10" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"></Image>
-          <Image className="overflow-hidden absolute -top-64 -right-80 -z-10" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"></Image>
-          <Image className="absolute hidden md:inline-block top-20 2xl:right-64 lg:right-32 right-12 lg:w-72 lg:h-72 w-56 h-56 -z-10" width={1000} height={1000} src={"/img/saturn.svg"} alt="saturn image" ></Image>
+        <Image className="overflow-hidden absolute -left-64 -z-10" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"/>
+        <Image className="overflow-hidden absolute -top-64 -right-80 -z-10" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"/>
+        <Image className="absolute hidden md:inline-block top-20 2xl:right-64 lg:right-32 right-12 lg:w-72 lg:h-72 w-56 h-56 -z-10" width={1000} height={1000} src={"/img/saturn.svg"} alt="saturn image" />
+          <Navbar/>
           {children}
-
         </div>
       </body>
     </html>
