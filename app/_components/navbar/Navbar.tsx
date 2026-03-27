9cfac83ddc13a522862493e24e75118442996c08
Maxim Tyuterev <maxtyuterev@gmail.com>
2026-03-27 11:44:30 -0400

    made navbar use flex

--- app/_components/navbar/Navbar.tsx
+++ app/_components/navbar/Navbar.tsx
@@ -6,28 +6,25 @@ import NavbarLink from "./displays/NavbarLink";
 export default function Navbar() {
 
   return (
-    // consider changing from fixed
-    <nav className="flex top-0 z-10 w-full h-16 bg-transparent">
-      {/* Replace with Logo and "Spotisize" that we can design */}
-      <div className="flex flex-row justify-between items-center h-fit w-full sm:pr-5 pr-1 pl-2 2xl:pr-">
-        <Link href="/" className="flex">
-          <Image
-            src={"/img/saturn.svg"}
-            alt="Home"
-            width={100}
-            height={100}
-            priority
-          />
-        </Link>
-
-        <div>
-          <ul className="flex flex-row justify-evenly items-center sm:pr-8 pr-4 font-jost">
-            <NavbarLink route="/" name="Home" ></NavbarLink>
-            <NavbarLink route="/about" name="About" ></NavbarLink>
-            <NavbarLink route="/contact" name="Contact Us" ></NavbarLink>
-          </ul>
-        </div>
-      </div>
+    <nav className="top-0 z-10 w-full h-16 bg-transparent">
+        <ul className="flex flex-row items-center sm:pr-8 pr-4 font-jost">
+          <li  className="flex " >
+            <Link href="/">
+              <Image
+                src={"/img/saturn.svg"}
+                alt="Home"
+                width={100}
+                height={100}
+                priority
+              />
+            </Link>
+          </li>
+          <div className="flex flex-row grow justify-end">
+          <li  className="flex" ><NavbarLink route="/" name="Home" ></NavbarLink></li>
+          <li  className="flex" ><NavbarLink route="/about" name="About" ></NavbarLink></li>
+          <li  className="flex" ><NavbarLink route="/contact" name="Contact Us" ></NavbarLink></li>
+          </div>
+        </ul>
     </nav>
   );
 }
