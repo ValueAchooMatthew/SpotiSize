ff238c4684068690d17943815616b1f25adf97cd
Maxim Tyuterev <maxtyuterev@gmail.com>
2026-04-05 05:27:43 -0400

    fixed background clipping weirdness

--- app/(routes)/layout.tsx
+++ app/(routes)/layout.tsx
@@ -3,6 +3,7 @@ import "../globals.css";
 import { Inter } from "next/font/google";
 import React from "react";
 import Image from "next/image";
+import noisesvg from "@/public/img/Noise.svg";
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
@@ -17,14 +18,12 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={inter.className + " bg-linear-to-b from-background-primary to-background-secondary"} >
-        <div className="flex flex-col grow">
-        <Image className="overflow-hidden absolute -left-64 -z-10" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"/>
-        <Image className="overflow-hidden absolute -top-64 -right-80 -z-10" width={1000} height={1000} src={"/img/Noise.svg"} alt="noise svg"/>
-        <Image className="absolute hidden md:inline-block top-20 2xl:right-64 lg:right-32 right-12 lg:w-72 lg:h-72 w-56 h-56 -z-10" width={1000} height={1000} src={"/img/saturn.svg"} alt="saturn image" />
+      <body className={inter.className + " min-h-screen bg-linear-to-b from-background-primary to-background-secondary"} >
+          <Image className="fixed -left-64 -z-10" src={noisesvg} loading="eager" alt=""/> 
+          <Image className="fixed -top-64 -right-80 -z-10" src={noisesvg} loading="eager" alt=""/>
           <Navbar/>
           {children}
-        </div>
+
       </body>
     </html>
   );
